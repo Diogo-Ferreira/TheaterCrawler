250ca7de04844c2cc71bd1af1b047367882507be
Diogo Ferreira <d.venancio.ferreira@gmail.com>
2018-10-24 12:33:34 +0200

    crawl of movie details

---
+++ crawler.js
@@ -0,0 +1,51 @@
+import fetch from 'node-fetch'
+import cheerio from 'cheerio'
+
+const host = 'https://www.cinevital.ch/'
+
+const timeout = ms => new Promise(res => setTimeout(res, ms))
+
+const fetchData = async (url) => {
+  const response = await fetch(url)
+  return await response.text()
+}
+
+const scrapeMoviesList = async (data) => {
+  const $ = cheerio.load(data)
+  const movieData = $(".movie-box > a").map((_, elem) => elem)
+
+  const movieDetails = await Promise.all(movieData.map(async (_, elem) => {
+    const name = $(elem).find('p').html()
+    const url = $(elem).attr('href')
+    await timeout(500)
+    return {[name]: await scrapeMovieDetails(`${host}/${url}`, name)}
+  }).get())
+
+  return movieDetails
+}
+
+const scrapeMovieDetails = async (url, movieName) => {
+  try {
+    const data = await fetchData(url);
+    const $ = cheerio.load(data)
+    const dayKey = (elem) => $(elem).find('.showdate h4').html().trim()
+    const findTime = (elem) => $(elem).find('button b').html().trim()
+    const findLocation = (elem) => $(elem).find('button br').get(0).nextSibling.nodeValue.toString().trim()
+    const showTimesPerDay = $('.showtimes .panel').map((_, elem) => ({
+      [dayKey(elem)]: `${findTime(elem)} : ${findLocation(elem)}`
+    })).get().reduce((a, b) => ({...a, ...b}), {})
+    return showTimesPerDay
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const crawl = async (url) => {
+  try {
+    const data = await fetchData(url)
+    const details = await scrapeMoviesList(data)
+    return details
+  } catch (error) {
+    console.log(error)
+  }
+}
--- index.js
+++ index.js
@@ -1,62 +1,33 @@
 
-import fetch from 'node-fetch'
-import cheerio from 'cheerio'
 
-const host = 'https://www.cinevital.ch/'
+import { crawl } from './crawler'
+import _ from 'lodash'
 
-const timeout = ms => new Promise(res => setTimeout(res, ms))
+let results = {};
 
-const fetchData = async (url) => {
-  const response = await fetch(url)
-  return await response.text()
-}
-
-const scrapeMoviesList = async (data) => {
-  const $ = cheerio.load(data)
-  const movieData = $(".movie-box > a").map((_, elem) => elem)
-
-  const movieDetails = await Promise.all(movieData.map(async (_, elem) => {
-    const name = $(elem).find('p').html()
-    const url = $(elem).attr('href')
-    await timeout(500)
-    return {[name]: await scrapeMovieDetails(`${host}/${url}`, name)}
-  }).get())
-
-  return movieDetails
-}
-
-const scrapeMovieDetails = async (url, movieName) => {
+const main = async (fakeData) => {
+  const host = 'https://www.cinevital.ch/'
   try {
-    const data = await fetchData(url);
-    const $ = cheerio.load(data)
-    const dayKey = (elem) => $(elem).find('.showdate h4').html().trim()
-    const findTime = (elem) => $(elem).find('button b').html().trim()
-    const findLocation = (elem) => $(elem).find('button br').get(0).nextSibling.nodeValue.toString().trim()
-    const showTimesPerDay = $('.showtimes .panel').map((_, elem) => ({
-      [dayKey(elem)]: `${findTime(elem)} : ${findLocation(elem)}`
-    })).get().reduce((a, b) => ({...a, ...b}), {})
-    return showTimesPerDay
+    const crawledData = await crawl(`${host}/fr/biel.html`)
+    const resultKeys = Object.keys(results)
+    const crawledDataKeys = Object.keys({...fakeData, ...crawledData})
+    if (resultKeys.length > 0) {
+      const diff = _.difference(crawledDataKeys, resultKeys)
+      if (diff.length > 0) {
+        console.log('Theres new data')
+      }
+    }
+
+    results = {...crawledData};
+
   } catch (error) {
     console.log(error)
   }
-}
-
-const crawl = async (url) => {
-  try {
-    const data = await fetchData(url)
-    const details = await scrapeMoviesList(data)
-    console.log(details)
-  } catch (error) {
-    console.log(error)
-  }
-}
-
+};
 (async () => {
-  try {
-    await crawl(`${host}/fr/biel.html`)
-  } catch (error) {
-    console.log(error)
-  }
+  await main()
+  await main({'coolMovie': {}})
 })()
+
 
 
