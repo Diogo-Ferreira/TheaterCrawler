aa971168ce3918ee6c8ea496f68c0c33402db3bc
Diogo Ferreira <d.venancio.ferreira@gmail.com>
2018-11-07 13:27:57 +0100

    first working version

--- crawler.js
+++ crawler.js
@@ -1,10 +1,6 @@
 import fetch from 'node-fetch'
 import cheerio from 'cheerio'
 
-const host = 'https://www.cinevital.ch/'
-
-const timeout = ms => new Promise(res => setTimeout(res, ms))
-
 const fetchData = async (url) => {
   const response = await fetch(url)
   return await response.text()
@@ -14,30 +10,11 @@ const scrapeMoviesList = async (data) => {
   const $ = cheerio.load(data)
   const movieData = $(".movie-box > a").map((_, elem) => elem)
 
-  const movieDetails = await Promise.all(movieData.map(async (_, elem) => {
-    const name = $(elem).find('p').html()
-    const url = $(elem).attr('href')
-    await timeout(500)
-    return {[name]: await scrapeMovieDetails(`${host}/${url}`, name)}
-  }).get())
+  const movieNames = movieData.map((_, elem) => {
+    return $(elem).find('p').html()
+  }).get()
 
-  return movieDetails
-}
-
-const scrapeMovieDetails = async (url, movieName) => {
-  try {
-    const data = await fetchData(url);
-    const $ = cheerio.load(data)
-    const dayKey = (elem) => $(elem).find('.showdate h4').html().trim()
-    const findTime = (elem) => $(elem).find('button b').html().trim()
-    const findLocation = (elem) => $(elem).find('button br').get(0).nextSibling.nodeValue.toString().trim()
-    const showTimesPerDay = $('.showtimes .panel').map((_, elem) => ({
-      [dayKey(elem)]: `${findTime(elem)} : ${findLocation(elem)}`
-    })).get().reduce((a, b) => ({...a, ...b}), {})
-    return showTimesPerDay
-  } catch (error) {
-    console.log(error)
-  }
+  return movieNames
 }
 
 export const crawl = async (url) => {
--- index.js
+++ index.js
@@ -3,10 +3,12 @@
 import { crawl } from './crawler'
 import _ from 'lodash'
 import moment from 'moment';
+import { onNewMovies } from './bot';
+import { timeout } from './utils';
 
-let currentMovies = {};
+let currentMovies = new Set();
 
-const main = async (startDate, endDate) => {
+const crawlBeetween = async (startDate, endDate) => {
   const host = 'https://www.cinevital.ch/'
   try {
     const crawledData = await crawl(`${host}/fr/biel.html?date=${startDate}&sdate=${endDate}`)
@@ -16,20 +18,59 @@ const main = async (startDate, endDate) => {
   }
 };
 
-(async () => {
-  const baseDate = moment();
+const main = async (days) => {
+  console.log('Starting crawling !')
+  const baseDate = moment()
   const baseDateStr = baseDate.format('YYYY-M-D')
-  const numberOfDays = 1;
+  const numberOfDays = days || 7
   
   const dates = Array(numberOfDays)
     .fill()
     .map(_ => baseDate.add(1, 'day').format('YYYY-M-D'))
 
-  const crawlings = dates.map(async date => await main(baseDateStr, date))
+  const crawlings = dates.map(async date => ({date, data: await crawlBeetween(baseDateStr, date)}))
 
   const results = await Promise.all(crawlings)
 
-  console.log(results)
+  const firstTime = currentMovies.size === 0
+
+  const {newMovies} =  compareResulstsAndGetNewMovies(currentMovies, results, firstTime);
+
+  currentMovies = new Set([...newMovies.map(i => i.movie), ...currentMovies])
+
+  if (newMovies.length > 0 && !firstTime) {
+    onNewMovies(newMovies)
+  }
+};
+
+export const compareResulstsAndGetNewMovies = (currentMovies, results) => {
+  let movies = new Set(currentMovies)
+  const newMovies = []
+  results.forEach(result => {
+    const { date, data } = result
+    data.forEach(movie => {
+      if (!movies.has(movie)) {
+        newMovies.push({firstDay: date, movie});
+      }
+      movies.add(movie)
+    })
+  })
+  return {
+    currentMovies,
+    newMovies
+  }
+}
+
+
+(async () => {
+  while(true) {
+    try {
+      await main(14)
+      await timeout(60 * 60000)
+    } catch (err) {
+      console.log(err)
+    }
+  }
 })()
 
 
